43194c6142eb7f1629a85ee946f35a36d99b588e
solver2005 <b_gafi@mail.ru>
2023-10-21 22:24:21 +0300

    first

---
+++ src/main/java/edu/project1/Hangman.java
@@ -0,0 +1,55 @@
+package edu.project1;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Hangman {
+    private Hangman() {
+    }
+
+    private static final int ATTEMPTS = 5;
+
+    @SuppressWarnings({"RegexpSinglelineJava", "UncommentedMain"})
+    public static void main(String[] args) {
+        String[] ar = {"rock", "december", "hello", "man", "water", "golf"};
+        Random rand = new Random();
+        int n = rand.nextInt(5);
+        char[] guessedSymbols = ar[n].toCharArray();
+        int wordSize = guessedSymbols.length;
+        char[] guess = new char[wordSize];
+        Arrays.fill(guess, '*');
+        Scanner sc = new Scanner(System.in);
+        int mistakes = 0;
+        while (mistakes < ATTEMPTS) {
+            System.out.println("Guess a letter: ");
+            String playerInp = sc.nextLine();
+            if (playerInp.length() != 1) {
+                System.out.println("Wrong input");
+                continue;
+            }
+            if (new String(guessedSymbols).contains(playerInp)) {
+                while (new String(guessedSymbols).contains(playerInp)) {
+                    int ind = new String(guessedSymbols).indexOf(playerInp);
+                    guess[ind] = guessedSymbols[ind];
+                    guessedSymbols[ind] = '#';
+                }
+                System.out.println(guess);
+                System.out.println("Hit!");
+            } else {
+                mistakes++;
+                System.out.println("Missed, mistake " + mistakes + " out of 5.");
+            }
+            System.out.println();
+            System.out.println("The word: " + new String(guess));
+            System.out.println();
+            if (!(new String(guess).contains("*"))) {
+                System.out.println("You won!");
+                break;
+            }
+        }
+        if (mistakes == ATTEMPTS) {
+            System.out.println("You lost!");
+        }
+    }
+}
